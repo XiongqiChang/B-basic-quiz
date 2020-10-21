e7476717c9e61f339f5db80c9d3a77bb27d41da9
XiongqiChang <2838159948@qq.com>
2020-10-21 13:19:52 +0800

    initproject

---
+++ src/test/java/com/thoughtworks/basictest/BasictestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.basictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasictestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
